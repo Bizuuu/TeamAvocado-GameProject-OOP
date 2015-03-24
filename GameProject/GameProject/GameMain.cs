f6d0a7fd369b5ba34ed6448cddc11ab4512ffd7e
Stoyan <stoyan.berov@gmail.com>
2015-03-24 20:41:31 +0200

    First Commit - Stoyan

---
+++ GameProject/GameProject/GameMain.cs
@@ -0,0 +1,12 @@
+﻿namespace GameProject
+{
+    using System;
+    
+    public class GameMain
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Stoyan was here");
+        }
+    }
+}

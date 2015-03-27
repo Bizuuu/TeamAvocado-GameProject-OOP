959b4a85b013943ed5243d064598b7ac582cb038
Stoyan <stoyan.berov@gmail.com>
2015-03-27 23:35:20 +0200

    Extra stuff for enemy

--- GameProject/GameProject/Enemy.cs
+++ GameProject/GameProject/Enemy.cs
@@ -32,7 +32,67 @@ namespace GameProject
             currentDifficultyLevel = 1;// later constants;
             bulletDelay = 40;// later a variable, changing according to the level;
 
-            // The rest is for the next on the list :)
+            speed = 5;
+            isVisible = true;
+        }
+
+        // Update
+        public void Update(GameTime gameTime)
+        {
+            // Update Collision Rectangle
+            boundingBox = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+
+            // Update Enemy Movement
+            position.Y += speed;
+
+            // Move Enemy back to top of the screen if he flies off bottom
+            if(position.Y >= 950)
+            {
+                position.Y = -75;
+            }
+
+        }
+        
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw enemy ship
+            spriteBatch.Draw(texture, position, Color.White);
+
+            // Draw enemy bullets
+            foreach (Bullet b in bulletList)
+            {
+                b.Draw(spriteBatch);
+            }
+        }
+
+        public void UpdateBullets()
+        {
+            // For each bullet in the bullet list update movement, and if the bullet hits the top of the screen remove from list;
+            foreach (var bullet in bulletList) // field List<Bullet> bulletList is to be implemented by someone else;
+            {
+                // bounding box for every bullet in the bulletList;
+                bullet.boundingBox = new Rectangle((int)bullet.position.X, (int)bullet.position.Y,
+                    bullet.texture.Width, bullet.texture.Height);
+
+                // set movement for bullet - someone else will do it;
+                b.position.Y = b.position.Y - b.speed;
+
+                // if bullet hits the top od the screen - make visible false - 
+                if (b.position.Y <= 0)
+                {
+                    b.isVisible = false;
+                }
+            }
+
+            // Iterate through the bulletList and if some bullet is not visible - remove it from list
+            for (int i = 0; i < bulletList.Count; i++)
+            {
+                if (!bulletList[i].isVisible)
+                {
+                    bulletList.RemoveAt(i);
+                    i--;
+                }
+            }
         }
     }
 }

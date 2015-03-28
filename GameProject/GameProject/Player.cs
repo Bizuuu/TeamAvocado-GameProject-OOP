c1f58a8475e074d85d916614cea082fc3089366f
shunobaka <vesselin_minev@abv.bg>
2015-03-28 16:34:36 +0200

    moved used namespaces

--- GameProject/GameProject/Player.cs
+++ GameProject/GameProject/Player.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-
-
-namespace GameProject
+﻿namespace GameProject
 {
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Content;
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework.Input;
+
     public class Player
     {
         public Texture2D texture;

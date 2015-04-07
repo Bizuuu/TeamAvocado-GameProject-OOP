033ef78cc4fce97bf6b26057a489521321a68d1e
shunobaka <vesselin_minev@abv.bg>
2015-04-07 11:46:28 +0300

    Made folders

---
+++ GameProject/GameProject/Models/Bonuses/BonusHealth.cs
@@ -0,0 +1,24 @@
+﻿namespace GameProject.Models
+{
+    using GameProject.Interfaces;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class BonusHealth : BonusObject, IMovableObject, ICollidable, IDestructable
+    {
+        private const int HealthBoost = 50;
+        private const int HealthBonusSpeed = 3;
+
+        public BonusHealth(Texture2D texture, Vector2 position) : base(texture, position, HealthBonusSpeed)
+        {
+        }
+
+        public override void DistributeBonusEffect(Player p)
+        {
+            if (p.Health < Player.InitialPlayerHealth)
+            {
+                p.Health += HealthBoost;
+            }
+        }
+    }
+}
---
+++ GameProject/GameProject/Models/Enemies/CommandoEnemy.cs
@@ -0,0 +1,22 @@
+﻿namespace GameProject.Models
+{
+    using GameProject.Interfaces;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class CommandoEnemy : Enemy, IEnemy, IShooting, IMovableObject, ICollidable, IRenderable 
+    {
+        private const int commandoEnemySpeed = 10;
+        private const int initialCommandoEnemyHealth = 5;
+        private const int commandoEnemyBulletDelay = 80;
+        private const int commandoEnemyBulletSpeed = 30;
+        private const int commandoEnemyPoints = 20;
+        private const int commandoEnemyShipDamage = 40;
+
+        public CommandoEnemy(Texture2D texture, Vector2 position, Texture2D EnemyBulletTexture)
+            : base(texture, position, EnemyBulletTexture, commandoEnemySpeed, initialCommandoEnemyHealth, commandoEnemyBulletDelay, commandoEnemyBulletSpeed, commandoEnemyPoints, commandoEnemyShipDamage)
+        {
+
+        }
+    }
+}
